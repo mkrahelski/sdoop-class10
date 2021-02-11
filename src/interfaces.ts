9e0d745b1508b27c9ce6de8eb67f3792850cfa71
Colby <cboettcher16590@alumni.focuscollege.com>
2021-02-10 20:01:35 -0800

    class11 sdoop

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,32 +1,72 @@
+export interface Entity {
+    name:string
+}
+export interface IPerson {
+    name:string;
+    age:number;
+    haveAPet: Entity [];
 
-
-// export interface Entity {
-//     type:string;
-
-// }
-
-export interface Ifamily {
-    People:Iperson[];
 }
 
-
-export interface Iperson {
-    PersonName:string;
-    age:number;
+export interface IFamily {
+    familyMembers:Entity[];
+    name:string
+    haveAPet: Entity [];
 
 }
 
 // Business (is an Entity)
-    // has owner
-export interface IBuissness{
-    BuisnessName:string;
-    owner:string[];
+// has owner
+export interface IBusiness{
+    businessName:Entity[];
+    owner_s:string[];
 
 }
 
 // Farm (is a Location, is a Business)
-export interface IFarm extends IBuissness{
+export interface IFarm {
 
-    FarmLocation:string;
+    farmLocation:string;
+    name:string;
 
 }
+
+export interface IAnimal {
+    size: string;
+    name:string;
+    isPet: boolean;
+}
+
+
+
+//Vehicles
+
+// Truck (is a Vehicle, can tow)
+
+// Car (is a Vehicle, cannot tow)
+
+//Trailers
+
+// Horse Trailer (can hold 2 large animals)
+
+// Cattle Trailer (can hold 20 Large Animals)
+
+// Small Trailer (can hold 1 large animal)
+
+export interface IVehicle {
+   towing:boolean;
+
+}
+
+export interface ITrailer{
+    trailerSize:number;
+   //how many animals can fit
+    animalWeight:number;
+}
+// export interface ICar extends IVehicle{
+//
+// }
+// export interface ITruck extends IVehicle{
+
+
+//}
